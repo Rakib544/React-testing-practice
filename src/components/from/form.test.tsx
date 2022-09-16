f42bf29fa167986b11a7f11703778bb7e219bfa9
Rakib544 <md.rakib10122003@gmail.com>
2022-09-16 12:13:01 +0600

    learning more about getByRole query

--- src/components/from/form.test.tsx
+++ src/components/from/form.test.tsx
@@ -4,8 +4,26 @@ import From from "./form";
 describe("From", () => {
   test("renders correctly", () => {
     render(<From />);
-    const nameElement = screen.getByRole("textbox");
+
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const pageSubHeading = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(pageSubHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
     expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bioElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
--- src/components/from/form.tsx
+++ src/components/from/form.tsx
@@ -1,28 +1,36 @@
 const Form = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div>
-        <label htmlFor="job-description">Job location</label>
-        <select id="job-location">
-          <option value="">Select A Country</option>
-          <option value="US">United State</option>
-          <option value="GB">United Kingdom</option>
-          <option value="CA">Canada</option>
-          <option value="BN">Bangladesh</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" id="terms" /> I agree to the terms and
-          conditions
-        </label>
-      </div>
-      <button>Submit</button>
-    </form>
+    <>
+      <h1>Admission Form</h1>
+      <h2>Step - 1</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" />
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea id="bio"></textarea>
+        </div>
+        <div>
+          <label htmlFor="job-description">Job location</label>
+          <select id="job-location">
+            <option value="">Select A Country</option>
+            <option value="US">United State</option>
+            <option value="GB">United Kingdom</option>
+            <option value="CA">Canada</option>
+            <option value="BN">Bangladesh</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" /> I agree to the terms and
+            conditions
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
   );
 };
 
